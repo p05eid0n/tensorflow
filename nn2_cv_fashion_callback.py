379b53eb08fa25ff33c78d19fdcbf4da86f57a42
Hitesh P <50610766+p05eid0n@users.noreply.github.com>
2019-05-21 09:54:16 +0530

    Update nn2_cv_fashion_callback.py

--- nn2_cv_fashion_callback.py
+++ nn2_cv_fashion_callback.py
@@ -11,12 +11,16 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 callbacks = mycallbacks()
-dataset = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = dataset.load_data()
 
 train_images = train_images/255
 test_images = test_images/255
 
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10,activation=tf.nn.softmax)
+])
 
 model.compile(optimizer=tf.train.AdamOptimizer(), loss='sparse_categorical_crossentropy')
 model.fit(train_images, train_labels, epochs=5, callbacks = [callbacks])
